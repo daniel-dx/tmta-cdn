bdb3275bfb8afa1a4af277eb535edf8570740fb4
daniel-dx <danieldx666@126.com>
2019-07-05 23:58:22 +0800

    Updates

---
+++ widgets/tmta-btn-widget/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h2>Demo Place</h2>
+
+    <div class="comp-wrapper">
+      <Btn :config="config"/>
+    </div>
+    
+    <ncform
+      :form-schema="configSchema"
+      v-model="config"
+      form-name="configForm"
+    ></ncform>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  .comp-wrapper {
+    border: 1px dashed #dcdfe6;
+    padding: 20px 0;
+    border-left: none;
+    border-right: none;
+  }
+}
+</style>
+
+<script>
+import Btn from "./components/Btn.vue";
+import configSchema from "../config-schema.js";
+
+export default {
+  name: "app",
+  components: {
+    Btn
+  },
+  data() {
+    return {
+      config: {
+        msg: "Hello Daniel"
+      },
+      configSchema
+    };
+  }
+};
+</script>
---
+++ widgets/tmta-btn-widget/src/components/Btn.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-button
+    :size="mergedConfig.size"
+    :type="mergedConfig.type"
+    :plain="mergedConfig.plain"
+    :round="mergedConfig.round"
+    :circle="mergedConfig.circle"
+    :icon="mergedConfig.icon"
+    class="btn">{{mergedConfig.label}}</el-button>
+</template>
+
+<style scoped lang="scss">
+.btn {
+}
+</style>
+
+<script>
+import { compMixin } from "tmta-core";
+
+export default {
+  name: "Btn",
+  mixins: [compMixin],
+  i18nData: {},
+  data() {
+    return {
+      defaultConfig: {
+        label: "Button",
+        size: "small",
+        type: "primary",
+        plain: false,
+        round: false,
+        circle: false,
+        icon: ""
+      }
+    };
+  }
+};
+</script>
